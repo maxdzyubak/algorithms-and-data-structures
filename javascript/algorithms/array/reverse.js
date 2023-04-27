fe07023164a22d8d5f10e8589db485cfeda1d9a5
Maxim Dzyubak <maxdzyubak@gmail.com>
2023-04-27 20:46:09 +0300

    Add reverse.js

---
+++ javascript/algorithms/array/reverse.js
@@ -0,0 +1,11 @@
+const reverse = (array) => {
+  const coll = array;
+  const lastIndex = coll.length - 1;
+  const middleIndex = lastIndex / 2;
+  for (let i = 0; i < middleIndex; i += 1) {
+    const mirrorIndex = lastIndex - i;
+    const temp = coll[i];
+    coll[i] = coll[mirrorIndex];
+    coll[mirrorIndex] = temp;
+  }
+};
